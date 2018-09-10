3b4fc525286c0abdca2a9bc0c10523a9df280a12
Ellen Schofield <tottlefields@gmail.com>
2018-09-10 15:40:51 +0100

    Updates for dog data

--- canvasDB_config.R
+++ canvasDB_config.R
@@ -14,13 +14,22 @@ if(!file.exists(tmpfileDir)){
     dir.create(tmpfileDir)
 }
 
-indbDatabaseName <- "canvasdb_test"
-annotationDatabaseName <- "canvasdb_test_annot_hg19"
+#indbDatabaseName <- "canvasdb_test"
+#annotationDatabaseName <- "canvasdb_test_annot_hg19"
+indbDatabaseName <- "es_canvasdb"
+annotationDatabaseName <- "es_canvasdb_annot_cf3"
 
-ANNOVARpath <- "/Volumes/Data/tools/annovar/annotate_variation.pl" ## Add path to ANNOVAR executable
-ANNOVARpathDB <- "/Volumes/Data/tools/annovar/humandb/" ## Add path to ANNOVAR annotation database
 
-dbSNPversion <- 137
+#ANNOVARpath <- "/Volumes/Data/tools/annovar/annotate_variation.pl" ## Add path to ANNOVAR executable
+#ANNOVARpathDB <- "/Volumes/Data/tools/annovar/humandb/" ## Add path to ANNOVAR annotation database
+ANNOVARpath <- "/Users/Ellen/Git/CanvasDB/annovar/annotate_variation.pl" ## Add path to ANNOVAR executable
+ANNOVARpathDB <- "/Users/Ellen/Git/CanvasDB/annovar/dogdb/" ## Add path to ANNOVAR annotation database
+
+VEPpath <- "/Users/Ellen/Git/ensembl-vep/vep"
+
+
+dbSNPversion <- 151
+eVersion <- 92
 
 mysqlConfigFile <- ".my.cnf"  # File containing MySQL user info
 
@@ -30,9 +39,11 @@ mysqlEngine <- "MyISAM"
 annotTables <- list()
 annotTables[["score"]] <- paste(annotationDatabaseName,".annot_score",sep="")
 annotTables[["dbsnp"]] <- paste(annotationDatabaseName,".annot_snp",dbSNPversion,"_single",sep="")
-annotTables[["dbsnpCommon"]] <- paste(annotationDatabaseName,".annot_snp",dbSNPversion,"_single_common",sep="")
+#annotTables[["dbsnpCommon"]] <- paste(annotationDatabaseName,".annot_snp",dbSNPversion,"_single_common",sep="")
 annotTables[["dbsnpIndels"]] <- paste(annotationDatabaseName,".annot_snp",dbSNPversion,"_indels",sep="")
-annotTables[["dbsnpCommonIndels"]] <- paste(annotationDatabaseName,".annot_snp",dbSNPversion,"_indels_common",sep="")
+#annotTables[["dbsnpCommonIndels"]] <- paste(annotationDatabaseName,".annot_snp",dbSNPversion,"_indels_common",sep="")
+annotTables[["vepSNP"]] <- paste(annotationDatabaseName,".annot_vep",eVersion,"_single",sep="")
+annotTables[["vepIndels"]] <- paste(annotationDatabaseName,".annot_vep",eVersion,"_indels",sep="")
 
 
 dbTables <- list()
